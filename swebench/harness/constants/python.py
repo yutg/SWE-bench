32e04535342f5b5b25f76de961e7beb6bab2c18e
lorgu <15050522059@163.com>
2025-02-11 12:11:47 +0800

    test

--- swebench/harness/constants/python.py
+++ swebench/harness/constants/python.py
@@ -589,9 +589,6 @@ for k in ["v5.3"]:
 SPECS_GYT = {
     k: {
         "python": "3.11",
-        "pip_packages": [
-            "pytest",
-        ],
         "test_cmd": TEST_GYT,
     }
     for k in ["0.1", "1.0"]
